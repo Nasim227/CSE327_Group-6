8faec2f78b09af4343ac433ba129de08a74f7bdc
MakkiSakib <makki.sakib@northsouth.edu>
2025-12-01 23:46:57 +0600

    modified

--- project/routes/web.php
+++ project/routes/web.php
@@ -2,5 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 
 Route::get('/', [ProductController::class, 'show_products'])->name('products.list');
+Route::post('/cart/add', [CartController::class, 'addToCart'])->name('cart.add');
+Route::get('/cart', [CartController::class, 'viewCart'])->name('cart.view');
+
+Route::post('/cart/increase/{id}', [CartController::class, 'increaseQuantity'])->name('cart.increase');
+Route::post('/cart/decrease/{id}', [CartController::class, 'decreaseQuantity'])->name('cart.decrease');
+Route::post('/cart/remove/{id}', [CartController::class, 'removeItem'])->name('cart.remove');
