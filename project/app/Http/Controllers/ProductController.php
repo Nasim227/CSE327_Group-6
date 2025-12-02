7fafb210b209d5bfb3b249ed7a8535a362334388
MakkiSakib <makki.sakib@northsouth.edu>
2025-12-02 19:33:48 +0600

    modified

--- project/app/Http/Controllers/ProductController.php
+++ project/app/Http/Controllers/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+
+
 /**
  * Class ProductController
  *
--- project/app/Models/Product.php
+++ project/app/Models/Product.php
@@ -4,12 +4,52 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
+/**
+ * Class Product
+ *
+ * Represents a product in the online store.
+ * 
+ * This model interacts with the 'products' table and allows the application
+ * to retrieve, display, and manage product information such as name, category,
+ * brand, price, images, and available quantity. It is used throughout the
+ * application wherever product data is needed.
+ *
+ * @package App\Models
+ */
 class Product extends Model
 {
+
+    /**
+     * The name of the database table this model represents.
+     *
+     * @var string
+     */
     protected $table = 'products';
+
+
+    /**
+     * The primary key for the products table.
+     *
+     * @var string
+     */
     protected $primaryKey = 'Product_id';
+
+
+    /**
+     * Determines if Laravel should automatically keep track of when products
+     * are created or updated.
+     *
+     * @var bool
+     */
     public $timestamps = false;
 
+
+    /**
+     * The attributes that can be set all at once when creating or updating a product.
+     *
+     * @var array
+     */
     protected $fillable = [
         'Product_name',
         'Category_name',
